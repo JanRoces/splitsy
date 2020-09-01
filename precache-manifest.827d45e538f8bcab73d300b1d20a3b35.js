aa6141ac23a1af999bcca5e07d23da4cabd59d06
Jan Roces <rocesjg@gmail.com>
2020-08-31 23:37:12 -0700

    Updates

---
+++ precache-manifest.827d45e538f8bcab73d300b1d20a3b35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c78bff0096d9cc7249db218f4fbcb5",
+    "url": "/splitsy/index.html"
+  },
+  {
+    "revision": "3b4ce20f5e5f115813dd",
+    "url": "/splitsy/static/css/main.45df5e87.chunk.css"
+  },
+  {
+    "revision": "22210ea5a0f18e83e0c5",
+    "url": "/splitsy/static/js/2.21a511d1.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/splitsy/static/js/2.21a511d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4ce20f5e5f115813dd",
+    "url": "/splitsy/static/js/main.8ea73b28.chunk.js"
+  },
+  {
+    "revision": "bf706ac5d4b70e1d6e36",
+    "url": "/splitsy/static/js/runtime-main.d357c11d.js"
+  }
+]);
