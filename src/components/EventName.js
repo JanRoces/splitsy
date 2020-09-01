8cb81936166a27d208384b277e8bf11b9dcaaf96
Jan Roces <rocesjg@gmail.com>
2020-09-01 01:49:13 -0700

    changed participant input

--- src/components/EventName.js
+++ src/components/EventName.js
@@ -8,47 +8,43 @@ class EventName extends Component {
     return (
       <div className="form-container">
         <form className="ui form" onSubmit={this.handleOnSubmit}>
-          <div className="field">
-            <input
-              type="text"
-              placeholder="Event Name"
-              onChange={(e) =>
-                this.setState({ title: e.target.value })
-              }></input>
-          </div>
-          <div className="field">
-            <input
-              type="text"
-              placeholder="Participant"
-              value={this.state.name}
-              onChange={(e) => this.setState({ name: e.target.value })}></input>
-          </div>
           <div>
-            <div className="generic-container">
+            <div className="field">
+              <input
+                type="text"
+                placeholder="Event Name"
+                onChange={(e) =>
+                  this.setState({ title: e.target.value })
+                }></input>
+            </div>
+
+            <div className="field">
+              <input
+                type="text"
+                placeholder="Participant"
+                value={this.state.name}
+                onChange={(e) =>
+                  this.setState({ name: e.target.value })
+                }></input>
+            </div>
+            <div className="field">
               <button
                 className="ui button"
                 type="button"
                 onClick={this.addParticipant}>
+                <i className="user plus icon"></i>
                 Add
               </button>
-              <button
-                className="ui button"
-                type="button"
-                onClick={this.removeParticipant}>
-                Remove
-              </button>
+            </div>
+          </div>
+          <div>
+            <br />
+            <div className="generic-container">
+              <div className="ui large labels">{this.showParticipants()}</div>
             </div>
             <br />
             <div className="generic-container">
-              <h4>Participants: </h4>
-              <br />
-            </div>
-            <div className="generic-container">
-              <p>{this.state.participants.join(", ")}</p>
-            </div>
-            <br />
-            <div className="generic-container">
-              <button className="ui button">Create</button>
+              <button className="ui primary button">Create</button>
             </div>
           </div>
         </form>
@@ -74,12 +70,44 @@ class EventName extends Component {
     }
   };
 
-  removeParticipant = () => {
-    if (this.state.participants !== []) {
-      let arr = this.state.participants;
-      arr.pop();
-      this.setState({ participants: arr });
+  removeParticipant = (name) => {
+    console.log(name);
+    var p = this.state.participants;
+    var len = p.length;
+    var i;
+
+    for (i = 0; i < len; i++) {
+      if (p[i] === name) {
+        p.splice(i, 1);
+        break;
+      }
     }
+
+    this.setState({ participants: p });
+  };
+
+  showParticipants = () => {
+    var p = this.state.participants;
+    var len = p.length;
+    var list = [];
+    var i;
+    var num;
+    var name;
+
+    for (i = 0; i < len; i++) {
+      name = p[i];
+      num = Math.floor(1000 + Math.random() * 9000);
+      list.push(
+        <div className="ui blue basic label" key={num}>
+          {p[i]}{" "}
+          <i
+            className="delete icon"
+            onClick={this.removeParticipant.bind(this, name)}></i>
+        </div>
+      );
+    }
+
+    return list;
   };
 }
 
