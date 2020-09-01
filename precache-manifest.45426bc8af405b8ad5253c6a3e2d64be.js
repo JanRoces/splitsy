5e1261a0b53024acb14e165ff473c374b7faedaf
Jan Roces <rocesjg@gmail.com>
2020-09-01 01:51:11 -0700

    Updates

--- precache-manifest.45426bc8af405b8ad5253c6a3e2d64be.js
+++ precache-manifest.45426bc8af405b8ad5253c6a3e2d64be.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "665ecb7205fc7bc1b89797823fedbbc5",
+    "revision": "b643abf5ea873dc64b92d743b4f8706a",
     "url": "/splitsy/index.html"
   },
   {
-    "revision": "9ec7f4fe56e88ce7f1b9",
+    "revision": "38995e2f4fb6c7365534",
     "url": "/splitsy/static/css/main.73bca0dd.chunk.css"
   },
   {
-    "revision": "22210ea5a0f18e83e0c5",
-    "url": "/splitsy/static/js/2.21a511d1.chunk.js"
+    "revision": "e69564cc25c44bb2cf6a",
+    "url": "/splitsy/static/js/2.cbfee823.chunk.js"
   },
   {
     "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
-    "url": "/splitsy/static/js/2.21a511d1.chunk.js.LICENSE.txt"
+    "url": "/splitsy/static/js/2.cbfee823.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9ec7f4fe56e88ce7f1b9",
-    "url": "/splitsy/static/js/main.1022005b.chunk.js"
+    "revision": "38995e2f4fb6c7365534",
+    "url": "/splitsy/static/js/main.e79b26d0.chunk.js"
   },
   {
     "revision": "bf706ac5d4b70e1d6e36",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/splitsy/precache-manifest.c0add3aa3963ad14e7c473addcf21769.js"
+  "/splitsy/precache-manifest.45426bc8af405b8ad5253c6a3e2d64be.js"
 );
 
 self.addEventListener('message', (event) => {
