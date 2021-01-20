ece1e92cdd9db5de0ab5b3712dc1e064820046f5
Jan Roces <rocesjg@gmail.com>
2021-01-19 21:22:56 -0800

    implemented signup and login

--- src/components/Main.js
+++ src/components/Main.js
@@ -72,11 +72,17 @@ class Main extends Component {
           <h3>Event Finance Organization</h3>
         </div>
         <div className="login-signup-container">
-          <div className="login-signup">
+          <div className="login-container">
             <h2>Log In</h2>
-            <input placeholder="User Name"></input>
-            <input placeholder="Password"></input>
-            <button></button>
+            <input className="login-input" placeholder="User Name"></input>
+            <input className="login-input" placeholder="Password"></input>
+            <button className="login-button">Sign In</button>
+          </div>
+          <div className="signup-container">
+            <h2>Sign Up</h2>
+            <input className="login-input" placeholder="Create User Name"></input>
+            <input className="login-input" placeholder="Create Password"></input>
+            <button className="signup-button">Sign Up</button>
           </div>
           {/* <div className="login-signup">sign up</div> */}
         </div>
